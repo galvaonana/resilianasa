d3cf1a95a252d049b66c6b4b476a0571d1426e3f
Natalia Galvao <nreginagal@gmail.com>
2021-05-06 22:07:02 -0300

    modificacoes finais no projeto

--- script/espaco.js
+++ script/espaco.js
@@ -1,1 +1,24 @@
-function()
+//comeca aqui esta pinoia
+const API_KEY = "4Op1ChAWJ4orUEFOxZcyCn3DEfvHVU0ObUutsXfS"
+
+function getDate() {
+    let data = document.getElementById('dateSearch').value;
+    return data;
+}
+
+function call() {
+    let request = '';
+    request = `https://api.nasa.gov/planetary/apod?date=${getDate()}&api_key=${API_KEY}`;
+
+    fetch(request).then(res => res.json()).then(
+        function(response) {
+
+            p = document.getElementById("descricao");
+            p.innerHTML = response.explanation;
+
+            img = document.getElementById("spacePic");
+            img.src = response.url
+
+        }
+    );
+}
